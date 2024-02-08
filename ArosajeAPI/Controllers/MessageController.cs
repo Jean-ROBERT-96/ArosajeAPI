902430d9231dd33344ec58df4ac6b33864d76ee8
Jean ROBERT <jean_robert92@live.fr>
2024-02-08 12:53:57 +0100

    Ajout des controllers vierges.

---
+++ ArosajeAPI/Controllers/MessageController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ArosajeAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MessageController : ControllerBase
+    {
+    }
+}
