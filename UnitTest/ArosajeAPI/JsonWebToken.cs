b1d336426449339da288129f8a11849709669398
Jean ROBERT <jean_robert92@live.fr>
2024-02-22 12:27:17 +0100

    Ajout des premiers éléments lié aux tests unitaires.

---
+++ UnitTest/ArosajeAPI/JsonWebToken.cs
@@ -0,0 +1,17 @@
+﻿namespace UnitTest.ArosajeAPI
+{
+    public class JsonWebToken
+    {
+        [Fact]
+        public void Use_ValidId_ReturnToken()
+        {
+
+        }
+
+        [Fact]
+        public void Use_InvalidId_ReturnError401()
+        {
+
+        }
+    }
+}
